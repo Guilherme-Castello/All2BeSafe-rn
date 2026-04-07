3d566ada9b8c0af3cc6950f92074133c6487976c
Guilherme Castello <guiaugusto.castello@gmail.com>
2026-04-07 14:58:31 -0300

    Feat: Implemented auto login

--- Types/UserStructure.ts
+++ Types/UserStructure.ts
@@ -1,7 +1,7 @@
 export type User = {
     _id: string,
     name: string,
-    password: string,
+    password: string | null | undefined,
     access_level: string,
     email: string,
     company: string
